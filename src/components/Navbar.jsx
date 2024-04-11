af43bf61e467bec9901fc03e644ac6844ed98053
Suraj <surajkumar78611@gmail.com>
2024-04-11 13:07:35 +0530

    Vhome name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
         to={"/"}
         className="w-10 h-10 rounded-lg bg-white flex items-center justify-center font-bold shadow-md"
       >
-        <p className="blue-gradient_text">3D PORTFOLIO</p>
+        <p className="blue-gradient_text">3D</p>
       </NavLink>
       <nav className="flex gap-7 text-lg font-medium">
         <NavLink
